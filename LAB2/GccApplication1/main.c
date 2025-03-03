d06267c23bc4f5707df0e0730e78fbb86d4a8da3
hedweger <hadwiget@gmail.com>
2025-03-03 10:23:05 +0100

    prep for lab3

--- LAB2/GccApplication1/main.c
+++ LAB2/GccApplication1/main.c
@@ -3,30 +3,30 @@
 
 #define BAUD 38400
 
-
-int main(void)
-{	
-	DDRB |= (1 << DDB5) | (1 << DDB6);  // Set PORTB pins 5 and 6 as output
-    DDRE |= (1 << DDE3);  // Set PORTE pin 3 as output
-	LED1OFF;
-	LED2OFF;
-	LED3OFF;
-	int j = 0;
-	UART_Init(BAUD);
-	UART_SendString("MENU:");
-	while(1)
-	{	
-		uint8_t recv = UART_GetChar();
-		if (recv == 49) {
-			UART_SendString("abcdefghijklmnopqrstuvwxyz\r\n");	
-		} else if (recv == 50) {
-			UART_SendString("ABCDEFGHIJKLMNOPQRSTUVWXYZ\r\n");
-		} else if (recv == 51) {
-			LED2CHANGE;
-			LED2CHANGE;
-			LED2CHANGE;
-		} else if (recv !=0) {
-			UART_SendString("unknown sequence\r\n");
-		}
-	} 
+int main(void) {
+  DDRB |= (1 << DDB5) | (1 << DDB6); // Set PORTB pins 5 and 6 as output
+  DDRE |= (1 << DDE3);               // Set PORTE pin 3 as output
+  LED1OFF;
+  LED2OFF;
+  LED3OFF;
+  int j = 0;
+  UART_Init(BAUD);
+  UART_SendString("MENU:");
+  UART_SendString("1: abeceda");
+  UART_SendString("2: ABECEDA");
+  UART_SendString("3: LED2 blik");
+  while (1) {
+    uint8_t recv = UART_GetChar();
+    if (recv == 49) {
+      UART_SendString("abcdefghijklmnopqrstuvwxyz\r\n");
+    } else if (recv == 50) {
+      UART_SendString("ABCDEFGHIJKLMNOPQRSTUVWXYZ\r\n");
+    } else if (recv == 51) {
+      LED2CHANGE;
+      LED2CHANGE;
+      LED2CHANGE;
+    } else if (recv != 0) {
+      UART_SendString("unknown sequence\r\n");
+    }
+  }
 }
--- LAB2/GccApplication1/uart/uart.c
+++ LAB2/GccApplication1/uart/uart.c
@@ -1,45 +1,40 @@
+#include <avr/interrupt.h>
 #include <avr/io.h>
 #include <stdint.h>
-#include <avr/interrupt.h>
 #define F_CPU 8000000 // 8 MHz internal clock
 
-void UART_Init(uint16_t baudrate)
-{
-	int ubbr = ((F_CPU/16/baudrate)-1);
-	UBRR1H = (uint8_t)(ubbr>>8);
-	UBRR1L = (uint8_t)ubbr;
-	// 
-	UCSR1B = (1<<RXCIE1)|(1<<TXCIE1)|(1<<RXEN1)|(1<<TXEN1);
+void UART_Init(uint16_t baudrate) {
+  int ubbr = ((F_CPU / 16 / baudrate) - 1);
+  UBRR1H = (uint8_t)(ubbr >> 8);
+  UBRR1L = (uint8_t)ubbr;
+  UCSR1B = (1 << RXCIE1) | (1 << TXCIE1) | (1 << RXEN1) | (1 << TXEN1);
 }
 
-
-void UART_SendChar(uint8_t data)
-{
-	while ( !( UCSR1A & (1<<UDRE1)) )
-	;
-	UDR1 = data;
+void UART_SendChar(uint8_t data) {
+  while (!(UCSR1A & (1 << UDRE1)))
+    ;
+  UDR1 = data;
 }
 
-void UART_SendString(char* text) {
-	for (int i=0; i < strlen(text); i++) {
-		if (text[i] == 0x00) {
-			return;
-		}
-		UART_SendChar(text[i]);
-	}
+void UART_SendString(char *text) {
+  for (int i = 0; i < strlen(text); i++) {
+    if (text[i] == 0x00) {
+      return;
+    }
+    UART_SendChar(text[i]);
+  }
 }
 
-uint8_t UART_GetChar( void )
-{
-	while ( !(UCSR1A & (1<<RXC1)) )
-	;
-	return UDR1;
+uint8_t UART_GetChar(void) {
+  while (!(UCSR1A & (1 << RXC1)))
+    ;
+  return UDR1;
 }
 
 ISR(USART1_RX_vect) {
-	uint8_t recv;
-	recv = UART_GetChar();
-	if (recv == 1) {
-		UART_SendChar('1');
-	}
+  uint8_t recv;
+  recv = UART_GetChar();
+  if (recv == 1) {
+    UART_SendChar('1');
+  }
 }
